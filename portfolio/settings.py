e3772d5aba7087a1766236fff94311a2fa088eef
RaviKishore94 <ravikishorespace@gmail.com>
2020-04-11 03:03:05 -0700

    new

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig', #class name from apps.py in that app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #folder to be created with name media
+
+MEDIA_URL = '/media/' #url path to fetch those files from media folder. (can be any name)
